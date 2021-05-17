d61e738b9842567179b6286f4262226a2e143a97
falcon9709 <83501247+falcon9709@users.noreply.github.com>
2021-05-17 23:35:56 +0800

    Update Subconverter.vue

--- src/views/Subconverter.vue
+++ src/views/Subconverter.vue
@@ -255,8 +255,7 @@ export default {
           "自动判断客户端": "auto",
         },
         customBackend: {
-          "pesy.ml 自用版": "http://pesy.ml/sub?",
-          "localhost:25500 本地版": "http://localhost:25500/sub?",
+          "guihui.tk自用版": "https://guihui.tk/sub?",
           "sub-beta.vercel.app": "https://sub-beta.vercel.app/sub?",
           "subcon.dlj.tf(subconverter作者提供-稳定)": "https://subcon.dlj.tf/sub?",
           "api.dler.io(sub作者&lhie1提供-稳定)": "https://api.dler.io/sub?",
@@ -264,7 +263,7 @@ export default {
           "sub.id9.cc(品云转换)": "https://sub.id9.cc/sub?",
         },
         backendOptions: [
-          { value: "http://pesy.ml/sub?" },
+          { value: "https://guihui.tk/sub?" },
           { value: "https://sub-beta.now.sh/sub?" },
           { value: "https://subcon.dlj.tf/sub?" },
           { value: "https://api.dler.io/sub?" },
